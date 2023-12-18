aac167f01091eda381b9576b811b3cd481748cc5
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-18 22:16:49 +0800

    fix flatpickr

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,8 +12,8 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-import flatpickr from "flatpickr";
-window.flatpickr = flatpickr;
+// import flatpickr from "flatpickr";
+// window.flatpickr = flatpickr;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,10 +11,13 @@
    <!-- Fonts -->
    <link rel="preconnect" href="https://fonts.bunny.net">
    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
    <!-- Scripts -->
    @vite(['resources/css/app.css', 'resources/js/app.js'])
    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
 
    <!-- Styles -->
    @livewireStyles
