3773d75c409659f54e56f6aa40080433b77423cf
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 02:20:00 +0800

    some udpate

--- app/Models/DanUser.php
+++ app/Models/DanUser.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DanUser extends Model
 {
@@ -22,4 +23,9 @@ class DanUser extends Model
         'gender',
         'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -40,18 +40,22 @@
                     @auth
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            @if (Auth::user()->danUser())
                             <button
                                 class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img src="data:image/png;base64,{{ Auth::user()->danUser()->image }}" alt="profile">
+                                {{-- <span class="mr-3">{{ Auth::user()->danUser()->firstname }}</span>
                                 <img class="h-8 w-8 rounded-full object-cover"
-                                    src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
                             </button>
                             @else
                             <span class="inline-flex rounded-md">
                                 <button type="button"
                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                    {{ Auth::user()->name }}
 
+                                    <span class="mr-3">{{ Auth::user()->name }}</span>
+                                    {{-- <img class="h-8 w-8 rounded-full object-cover"
+                                    src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" /> --}}
                                     <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round"
