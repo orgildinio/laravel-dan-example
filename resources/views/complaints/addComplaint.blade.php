65ac53a1bd9ccae7fe39b9b5e86e3d2e7a92f98f
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 01:30:54 +0800

    some udpate

--- resources/views/complaints/addComplaint.blade.php
+++ resources/views/complaints/addComplaint.blade.php
@@ -26,7 +26,7 @@
                         <div class="md:w-2/3">
                             <input
                                 class="bg-gray-200 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
-                                id="inline-full-name" type="text" name="lastname" value="{{$danUser->lastname }}">
+                                id="inline-full-name" type="text" name="lastname" value="{{isset($danUser) ? $danUser->lastname : ''}}">
                         </div>
                     </div>
                     <div class="md:flex md:items-center mb-2">
