874248add6cddea2e7fa658ecb6e26ec12e6a5d7
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-18 23:41:33 +0800

    some update

--- app/Http/Controllers/DanAuthController.php
+++ app/Http/Controllers/DanAuthController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\EnergyType;
+use App\Models\Organization;
 use Illuminate\Http\Request;
+use App\Models\ComplaintType;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -17,7 +21,13 @@ class DanAuthController extends Controller
     public function handleDanCallback()
     {
         $danUser = Socialite::driver('dan')->user();
-        dd($danUser);
+
+        $categories = Category::all();
+        $orgs = Organization::all();
+        $complaint_types = ComplaintType::all();
+        $energy_types = EnergyType::all();
+
+        // dd($danUser);
 
         // Check if the user already exists in your database
         // $user = User::where('email', $danUser->email)->first();
@@ -33,7 +43,7 @@ class DanAuthController extends Controller
 
         // Auth::login($user, true);
 
-        return redirect()->route("welcome")->with('success', 'Амжилттай нэвтэрлээ.');
-        return view('complaints.addComplaint', ['user' => $danUser]);
+        // return redirect()->route("welcome")->with('success', 'Амжилттай нэвтэрлээ.');
+        return view('complaints.addComplaint', compact('categories', 'orgs', 'complaint_types', 'energy_types', 'danUser'));
     }
 }
