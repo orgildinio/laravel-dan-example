f5eff4f7779f755f615057ce13cc13877c3be7cf
gantulgarus <gantulgarus@gmail.com>
2023-12-18 22:56:13 +0000

    some change

--- app/Providers/DanServiceProvider.php
+++ app/Providers/DanServiceProvider.php
@@ -63,7 +63,7 @@ class DanServiceProvider extends AbstractProvider implements ProviderInterface
      */
     protected function mapUserToObject(array $user)
     {
-        dd($user[0]);
+        //dd($user);
 
         return (new User())->setRaw($user)->map([
             'personId' => $user['personId'],
