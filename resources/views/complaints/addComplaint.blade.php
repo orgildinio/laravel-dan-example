9e46e8ee4c1fde64bb5d5f036b25b8289438dddf
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-18 23:44:39 +0800

    some update

--- resources/views/complaints/addComplaint.blade.php
+++ resources/views/complaints/addComplaint.blade.php
@@ -39,7 +39,7 @@
                         <div class="md:w-2/3">
                             <input
                                 class="bg-gray-200 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
-                                id="inline-first-name" type="text" name="firstname" value="">
+                                id="inline-first-name" type="text" name="firstname" value="{{$danUser->firstname}}">
                         </div>
                     </div>
                     <div class="md:flex md:items-center mb-2">
