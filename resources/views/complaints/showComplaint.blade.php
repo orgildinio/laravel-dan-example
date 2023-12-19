5d23a32e8775943f5d283bffa240604fcc627259
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 10:37:36 +0800

    some udpate

--- resources/views/complaints/showComplaint.blade.php
+++ resources/views/complaints/showComplaint.blade.php
@@ -29,11 +29,11 @@
                             <div class="flex items-start justify-between w-full">
                                 <div class="pl-3 w-full">
                                     <p tabindex="0" class="focus:outline-none text-xl font-medium leading-5 text-gray-800 ">
-                                        {{$complaint->category->name}} - №{{$complaint->id}}</p>
+                                        {{$complaint->category?->name}} - №{{$complaint->id}}</p>
                                     <p tabindex="0" class="focus:outline-none text-sm leading-normal pt-2 text-gray-500">
                                         {{$complaint->created_at}}</p>
                                     <p class="text-sm my-1"><span
-                                            class="bg-gray-300 p-1 rounded">{{$complaint->organization->name}} {{$complaint->status->name}}.</span></p>
+                                            class="bg-gray-300 p-1 rounded">{{$complaint->organization?->name}} {{$complaint->status?->name}}.</span></p>
                                 </div>
                                 <div role="img" aria-label="bookmark">
                                     <svg class="focus:outline-none text-gray-800" width="28" height="28" viewBox="0 0 28 28"
