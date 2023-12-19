492039e2b1ffe5c922bc4f4e9305b6361c924961
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 11:47:09 +0800

    add fix

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,8 @@ Route::middleware([
     Route::get('/complaintStatus/{id}', [ComplaintController::class, 'complaintStatus'])->name('complaintStatus');
     Route::post('/getOrg', [ComplaintController::class, 'getOrg']);
     // Route::post('/upload', [ComplaintController::class, 'upload'])->name('upload');
+    Route::get('/addComplaint', [ComplaintController::class, 'addComplaint'])->name('addComplaint');
 });
-Route::get('/addComplaint', [ComplaintController::class, 'addComplaint'])->name('addComplaint');
 Route::get('/complaints', [ComplaintController::class, 'complaints'])->name('complaints');
 Route::get('/userComplaints', [ComplaintController::class, 'userComplaints'])->name('userComplaints');
 Route::get('/showComplaint/{id}', [ComplaintController::class, 'showComplaint'])->name('showComplaint');
