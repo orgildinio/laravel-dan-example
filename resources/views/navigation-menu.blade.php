256d9ec6cf74e5f519831c14f2fd561fe7649640
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 08:35:24 +0800

    some update

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -40,11 +40,14 @@
                     @auth
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            {{-- @if (Laravel\Jetstream\Jetstream::managesProfilePhotos()) --}}
+                            @if (Auth::user()->danImage)
                             <button
                                 class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover"
-                                    src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" />
+                                {{-- <img class="h-8 w-8 rounded-full object-cover"
+                                    src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" /> --}}
+                                <span class="mr-3">{{ Auth::user()->name }}</span>
+                                <img src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile">
                                 {{-- <img class="h-8 w-8 rounded-full object-cover"
                                     src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
                             </button>
