fd9d839f22b69cca9cc5e714ed475dc1fd9e36bc
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2023-12-19 08:42:59 +0800

    user profile image on nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -40,14 +40,13 @@
                     @auth
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            {{-- @if (Laravel\Jetstream\Jetstream::managesProfilePhotos()) --}}
-                            @if (Auth::user()->danImage)
+                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                             <button
                                 class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                {{-- <img class="h-8 w-8 rounded-full object-cover"
-                                    src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" /> --}}
-                                <span class="mr-3">{{ Auth::user()->name }}</span>
-                                <img src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile">
+                                <img class="h-8 w-8 rounded-full object-cover"
+                                    src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" />
+                                {{-- <span class="mr-3">{{ Auth::user()->name }}</span>
+                                <img src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile"> --}}
                                 {{-- <img class="h-8 w-8 rounded-full object-cover"
                                     src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" /> --}}
                             </button>
@@ -57,7 +56,7 @@
                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
 
                                     <span class="mr-3">{{ Auth::user()->name }}</span>
-                                    <img src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile">
+                                    <img class="h-8 w-8 rounded-full object-cover" src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile">
                                     {{-- <img class="h-8 w-8 rounded-full object-cover"
                                     src="{{ asset('/image/user-profile-icon.svg')}}" alt="{{ Auth::user()->name }}" />
                                     <img src="data:image/png;base64,{{ Auth::user()->danUser()->image }}" alt="{{ $image->title }}"> --}}
