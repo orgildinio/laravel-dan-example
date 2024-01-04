bab0b10eb9738b69a7c2a67bd685e5f61b27c802
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-01-04 09:51:01 +0800

    some update

--- app/View/Components/SidebarMenu.php
+++ app/View/Components/SidebarMenu.php
@@ -26,12 +26,13 @@ class SidebarMenu extends Component
     public function render()
     {
         $org_id = Auth::user()->org_id;
+        $user_id = Auth::user()->id;
 
         $new_complaints = Complaint::where('status_id', 0)->where('organization_id', $org_id)->count();
-        $received_complaints = Complaint::where('status_id', 2)->where('organization_id', $org_id)->count();
-        $under_control_complaints = Complaint::where('status_id', 3)->where('organization_id', $org_id)->count();
-        $solved_complaints = Complaint::where('status_id', 6)->where('organization_id', $org_id)->count();
-        $canceled_complaints = Complaint::where('status_id', 4)->where('organization_id', $org_id)->count();
+        $received_complaints = Complaint::where('status_id', 2)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
+        $under_control_complaints = Complaint::where('status_id', 3)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
+        $solved_complaints = Complaint::where('status_id', 6)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
+        $canceled_complaints = Complaint::where('status_id', 4)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
         $all_complaints = Complaint::all()->count();
 
         return view('components.sidebar-menu', ['new_complaints' => $new_complaints, 'received_complaints' => $received_complaints, 'under_control_complaints' => $under_control_complaints, 'solved_complaints' => $solved_complaints, 'canceled_complaints' => $canceled_complaints, 'all_complaints' => $all_complaints]);
