de77d520a17c38a713623cfe94da6ecdcb8df923
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-01-12 10:10:41 +0800

    sidebar complaint count fix

--- app/View/Components/SidebarMenu.php
+++ app/View/Components/SidebarMenu.php
@@ -28,7 +28,18 @@ class SidebarMenu extends Component
         $org_id = Auth::user()->org_id;
         $user_id = Auth::user()->id;
 
-        $new_complaints = Complaint::where('status_id', 0)->where('organization_id', $org_id)->count();
+        // $new_complaints = Complaint::where('status_id', 0)->where('organization_id', $org_id)->count();
+
+        $new_complaints = Complaint::where(function ($query) {
+            $query->where('organization_id', Auth::user()->org_id)
+                ->orWhere('second_org_id', Auth::user()->org_id);
+        })
+            ->where(function ($query) {
+                $query->where('status_id', 0)
+                    ->orWhere('second_status_id', 0);
+            })
+            ->count();
+
         $received_complaints = Complaint::where('status_id', 2)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
         $under_control_complaints = Complaint::where('status_id', 3)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
         $solved_complaints = Complaint::where('status_id', 6)->where('organization_id', $org_id)->where('controlled_user_id', $user_id)->count();
