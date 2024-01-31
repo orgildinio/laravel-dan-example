8361b77613179cb945904178e4372c4efe5926f1
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-01-31 16:15:11 +0800

    some update

--- app/Exports/ExportComplaint.php
+++ app/Exports/ExportComplaint.php
@@ -51,42 +51,42 @@ class ExportComplaint implements FromCollection, WithHeadings, ShouldAutoSize, W
             ->join('organizations', 'complaints.organization_id', '=', 'organizations.id')
             ->join('energy_types', 'complaints.energy_type_id', '=', 'energy_types.id')
             ->join('complaint_maker_types', 'complaints.complaint_maker_type_id', '=', 'complaint_maker_types.id')
-            // ->select(
-            //     'categories.name as category',
-            //     'channels.name as channel',
-            //     'statuses.name as status',
-            //     'users.name as name',
-            //     'organizations.name as org',
-            //     'energy_types.name as energytype',
-            //     'complaint_maker_types.name as orgtype',
-            //     'complaints.lastname',
-            //     'complaints.firstname',
-            //     'complaints.complaint_maker_org_name',
-            //     'complaints.phone',
-            //     'complaints.complaint',
-            //     'complaints.complaint_date'
-            // )
-            // ->when(isset($_GET['daterange']), function ($query) {
-            //     $date_range = explode(' to ', $_GET['daterange']);
-            //     $date['start'] = \Carbon\Carbon::parse($date_range[0])->format('Y-m-d H:i:s');
-            //     $date['end'] = \Carbon\Carbon::parse($date_range[1])->format('Y-m-d H:i:s');
-            //     $query->whereBetween('complaint_date', [$date['start'], $date['end']]);
-            // })
-            // ->when(isset($_GET['search_text']), function ($query) {
-            //     $query->where('complaint', 'like', "%{$_GET['search_text']}%");
-            // })
-            // ->when(isset($_GET['status_id']), function ($query) {
-            //     $query->where('status_id', $_GET['status_id']);
-            // })
-            // ->when(isset($_GET['org_id']), function ($query) {
-            //     $query->where('organization_id', $_GET['org_id']);
-            // })
-            // ->when(Auth::user()->org_id != 99, function ($query) {
-            //     $query->where('organization_id', Auth::user()->org_id);
-            // })
-            // ->when(isset($_GET['energy_type_id']), function ($query) {
-            //     $query->where('energy_type_id', $_GET['energy_type_id']);
-            // })
+            ->select(
+                'categories.name as category',
+                'channels.name as channel',
+                'statuses.name as status',
+                'users.name as name',
+                'organizations.name as org',
+                'energy_types.name as energytype',
+                'complaint_maker_types.name as orgtype',
+                'complaints.lastname',
+                'complaints.firstname',
+                'complaints.complaint_maker_org_name',
+                'complaints.phone',
+                'complaints.complaint',
+                'complaints.complaint_date'
+            )
+            ->when(isset($_GET['daterange']), function ($query) {
+                $date_range = explode(' to ', $_GET['daterange']);
+                $date['start'] = \Carbon\Carbon::parse($date_range[0])->format('Y-m-d H:i:s');
+                $date['end'] = \Carbon\Carbon::parse($date_range[1])->format('Y-m-d H:i:s');
+                $query->whereBetween('complaint_date', [$date['start'], $date['end']]);
+            })
+            ->when(isset($_GET['search_text']), function ($query) {
+                $query->where('complaint', 'like', "%{$_GET['search_text']}%");
+            })
+            ->when(isset($_GET['status_id']), function ($query) {
+                $query->where('status_id', $_GET['status_id']);
+            })
+            ->when(isset($_GET['org_id']), function ($query) {
+                $query->where('organization_id', $_GET['org_id']);
+            })
+            ->when(Auth::user()->org_id != 99, function ($query) {
+                $query->where('organization_id', Auth::user()->org_id);
+            })
+            ->when(isset($_GET['energy_type_id']), function ($query) {
+                $query->where('energy_type_id', $_GET['energy_type_id']);
+            })
             ->orderBy('complaints.complaint_date', 'desc')
             ->get();
         dd($complaints);
