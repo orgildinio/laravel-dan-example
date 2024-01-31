fae07904fb81a930b1e3befd7639e8f67a52b83f
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-01-31 15:48:31 +0800

    some update

--- app/Exports/ExportComplaint.php
+++ app/Exports/ExportComplaint.php
@@ -48,7 +48,7 @@ class ExportComplaint implements FromCollection, WithHeadings, ShouldAutoSize, W
             ->join('channels', 'complaints.channel_id', '=', 'channels.id')
             ->join('statuses', 'complaints.status_id', '=', 'statuses.id')
             ->join('users', 'complaints.controlled_user_id', '=', 'users.id')
-            // ->join('organizations', 'complaints.organization_id', '=', 'organizations.id')
+            ->join('organizations', 'complaints.organization_id', '=', 'organizations.id')
             // ->join('energy_types', 'complaints.energy_type_id', '=', 'energy_types.id')
             // ->join('complaint_maker_types', 'complaints.complaint_maker_type_id', '=', 'complaint_maker_types.id')
             ->select(
@@ -56,7 +56,7 @@ class ExportComplaint implements FromCollection, WithHeadings, ShouldAutoSize, W
                 'channels.name as channel',
                 'statuses.name as status',
                 'users.name as name',
-                // 'organizations.name as org',
+                'organizations.name as org',
                 // 'energy_types.name as energy type',
                 // 'complaint_maker_types.name as orgtype',
                 'complaints.lastname',
