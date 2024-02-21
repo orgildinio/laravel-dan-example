d0074ca49ef4c1c02c89314a11a47edf70f7c48f
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-02-21 15:38:51 +0800

    update dashboard

--- resources/views/components/dashboard-tze.blade.php
+++ resources/views/components/dashboard-tze.blade.php
@@ -1,66 +1,82 @@
 <div class="mt-4">
     <section class="grid md:grid-cols-2 xl:grid-cols-4 gap-4 my-4">
-        <div class="flex items-center p-8 bg-white shadow rounded-lg">
-            <div
-                class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-8 h-8 rounded-full text-purple-700"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                </svg>
+        <div class="flex flex-col justify-between items-start p-8 bg-white shadow rounded-lg">
+            <div class="">
+                <span class="block text-gray-900 font-bold text-lg">Нийт өргөдөл, гомдол</span>
             </div>
-            <div>
-                <span class="block text-2xl font-bold">{{ $tze_tog + $tze_dulaan }}</span>
-                <span class="block text-gray-500">Нийт</span>
+            <div class="flex items-center">
+                <div
+                    class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-8 h-8 rounded-full text-purple-700"
+                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                    </svg>
+                </div>
+                <div>
+                    <span class="block text-4xl font-bold">{{ $tze_tog + $tze_dulaan }}</span>
+                </div>
             </div>
         </div>
-        <div class="flex items-center p-8 bg-white shadow rounded-lg">
-            <div
-                class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-8 h-8 rounded-full text-blue-700">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
-                </svg>
+        <div class="flex flex-col items-start p-8 bg-white shadow rounded-lg">
+            <div class="pb-4">
+                <span class="block text-gray-900 font-bold text-lg">Нийт өргөдөл, гомдол /Цахилгаан/</span>
+            </div>
+            <div class="flex items-center">
+                <div
+                    class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-8 h-8 rounded-full text-blue-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                    </svg>
+                </div>
+                <div>
+                    <span class="block text-4xl font-bold">{{ $tze_tog }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col justify-between items-start p-8 bg-white shadow rounded-lg">
+            <div class="">
+                <span class="block text-gray-900 font-bold text-lg">Нийт өргөдөл, гомдол /Дулаан/</span>
+            </div>
+            <div class="flex items-center">
+                <div
+                    class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-orange-600 bg-orange-100 rounded-full mr-6">
+    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-8 h-8 rounded-full text-orange-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+                    </svg>
+    
+                </div>
+                <div>
+                    <span class="inline-block text-4xl font-bold">{{ $tze_dulaan }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col justify-between items-start p-8 bg-white shadow rounded-lg">
+            <div class="">
+                <span class="block text-gray-900 font-bold text-lg">Хугацаа хэтэрсэн өргөдөл, гомдол</span>
+            </div>
+            <div class="flex items-center">
+                <div
+                    class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-red-600 bg-red-100 rounded-full mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-8 h-8 rounded-full text-red-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </div>
+                <div>
+                    <span class="block text-4xl font-bold">{{ $exp_comp }}</span>
+                </div>
+            </div>
+        </div>
 
-            </div>
-            <div>
-                <span class="block text-2xl font-bold">{{ $tze_tog }}</span>
-                <span class="block text-gray-500">Цахилгаан</span>
-            </div>
-        </div>
-        <div class="flex items-center p-8 bg-white shadow rounded-lg">
-            <div
-                class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-orange-600 bg-orange-100 rounded-full mr-6">
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-8 h-8 rounded-full text-orange-700">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
-                </svg>
-
-            </div>
-            <div>
-                <span class="inline-block text-2xl font-bold">{{ $tze_dulaan }}</span>
-                <span class="block text-gray-500">Дулаан</span>
-            </div>
-        </div>
-        <div class="flex items-center p-8 bg-white shadow rounded-lg">
-            <div
-                class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-red-600 bg-red-100 rounded-full mr-6">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-8 h-8 rounded-full text-red-700">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-            </div>
-            <div>
-                <span class="block text-2xl font-bold">{{ $exp_comp }}</span>
-                <span class="block text-gray-500">Хугацаа хэтэрсэн</span>
-            </div>
-        </div>
     </section>
     <section class="grid md:grid-cols-3 xl:grid-cols-3 gap-4">
         <div id="chartEnergyTypeTze" class="bg-white shadow rounded-lg"></div>
