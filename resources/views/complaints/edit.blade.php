4f4ec29fb87f3e3c926a355e9f601bde3b0000b7
Gantulga <gantulga@Gantulgas-MacBook-Pro.local>
2024-02-23 15:07:29 +0800

    complaint update error fix

--- resources/views/complaints/edit.blade.php
+++ resources/views/complaints/edit.blade.php
@@ -315,7 +315,7 @@
                         <div class="md:w-2/3">
                             <select name="complaint_type_summary_id" id="complaint_type_summary_id"
                                 class="bg-gray-200 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500">
-                                <option value='{{ $complaint->complaint_type_summary_id }}'>{{$complaint->complaintTypeSummary->name}}</option>
+                                <option value='{{ $complaint->complaint_type_summary_id }}'>{{$complaint->complaintTypeSummary?->name}}</option>
                             </select>
                         </div>
                     </div>
