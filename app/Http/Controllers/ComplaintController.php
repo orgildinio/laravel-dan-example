e4ffca38b74d3e8d986756f4cd9009709c820c44
gantulgarus <gantulgarus@gmail.com>
2024-03-19 18:02:38 +0800

    test finish

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -337,10 +337,7 @@ class ComplaintController extends Controller
                 break;
         }
 
-        // $query->where('status_id', $status_id)->where('organization_id', $org_id);
         $complaints = $query->orderBy('complaints.created_at', 'desc')->paginate(10);
-        // $complaints = Complaint::latest()->paginate(15);
-        // dd($complaints);
 
         $currentYear = date('Y');
         $years = range($currentYear, $currentYear - 5, -1);
