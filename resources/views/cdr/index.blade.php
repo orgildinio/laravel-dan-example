baac66b88de61f80c5e6347cf70fd053c7bcb346
gantulgarus <gantulgarus@gmail.com>
2024-03-19 14:22:36 +0800

    cdr index update

--- resources/views/cdr/index.blade.php
+++ resources/views/cdr/index.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/cdr/index.blade.php -->
 <x-admin-layout>
     <div class="container mx-auto px-4 py-8">
-        <h1 class="text-3xl font-bold mb-4">Cdr Records</h1>
+        <h1 class="text-3xl font-bold mb-4">Яриа бичлэг</h1>
 
         <div class="bg-white shadow-md rounded my-6">
             <table class="w-full table-auto">
@@ -11,7 +11,11 @@
                         <th class="py-3 px-6 text-left">Clid</th>
                         <th class="py-3 px-6 text-left">Src</th>
                         <th class="py-3 px-6 text-left">Dst</th>
-                        <!-- Add more table headers as needed -->
+                        <th class="py-3 px-6 text-left">Duration</th>
+                        <th class="py-3 px-6 text-left">Bill Sec</th>
+                        <th class="py-3 px-6 text-left">Disposition</th>
+                        <th class="py-3 px-6 text-left">Linked id</th>
+                        <th class="py-3 px-6 text-left">Audio</th>
                     </tr>
                 </thead>
                 <tbody class="text-gray-600 text-sm font-light">
@@ -21,7 +25,16 @@
                             <td class="py-3 px-6 text-left">{{ $record->clid }}</td>
                             <td class="py-3 px-6 text-left">{{ $record->src }}</td>
                             <td class="py-3 px-6 text-left">{{ $record->dst }}</td>
-                            <!-- Add more table cells as needed -->
+                            <td class="py-3 px-6 text-left">{{ $record->duration }}</td>
+                            <td class="py-3 px-6 text-left">{{ $record->billsec }}</td>
+                            <td class="py-3 px-6 text-left">{{ $record->disposition }}</td>
+                            <td class="py-3 px-6 text-left">{{ $record->linkedid }}</td>
+                            <td class="py-3 px-6 text-left">
+                                <audio controls class="w-full">
+                                    <source src="/opt/records/{{ $record->linkedid }}.wav" type="audio/mpeg">
+                                    Your browser does not support the audio tag.
+                                </audio>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
