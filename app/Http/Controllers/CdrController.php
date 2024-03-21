0c26af002c8798ba6c51b87917e3a0d68e4d8f84
gantulgarus <gantulgarus@gmail.com>
2024-03-21 14:58:35 +0800

    update

--- app/Http/Controllers/CdrController.php
+++ app/Http/Controllers/CdrController.php
@@ -19,8 +19,8 @@ class CdrController extends Controller
         $org_id = Auth::user()->org_id;
         $org_numbers = OrganizationNumbers::where('organization_id', $org_id)->get();
 
+        // dd($org_numbers);
         $cdrRecords = Cdr::orderBy('calldate', 'desc')->whereIn('src', $org_numbers)->paginate(15);
-        dd($cdrRecords);
 
         return view('cdr.index', compact('cdrRecords'));
     }
