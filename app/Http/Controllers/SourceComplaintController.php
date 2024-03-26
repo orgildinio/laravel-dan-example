ef0e2495f598dfa065c82688815905a375834902
gantulgarus <gantulgarus@gmail.com>
2024-03-26 13:34:20 +0800

    test 1111 connection

--- app/Http/Controllers/SourceComplaintController.php
+++ app/Http/Controllers/SourceComplaintController.php
@@ -19,9 +19,31 @@ class SourceComplaintController extends Controller
         $response = Http::get('https://www.11-11.mn/GStest/APIa', $params);
         // $response = Http::get('https://www.11-11.mn/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
         // $response = Http::get('http://103.87.69.87/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
-        // dd($response->json());
-        $results = $response->json();
+        dd($response->getBody()->getContents());
+        // $results = $response->json();
 
-        return $results;
+        // // Convert JSON string to PHP array
+        // $data = json_decode($jsonString, true);
+
+        // // Initialize an empty array for the converted data
+        // $convertedData = [];
+
+        // // Iterate through the 'smart' array to create the desired format
+        // foreach ($data['smart']['created_at'] as $key => $value) {
+        //     $convertedData[] = [
+        //         "created_at" => $data['smart']['created_at'][$key],
+        //         "source" => $data['smart']['source'][$key],
+        //         "quarter" => $data['smart']['quarter'][$key],
+        //         // Add other fields here
+        //     ];
+        // }
+
+        // // Encode the converted data back to JSON format if needed
+        // $convertedJson = json_encode($convertedData, JSON_PRETTY_PRINT);
+
+        // // Print or return the converted JSON data
+        // return $convertedJson;
+
+        // return $results;
     }
 }
