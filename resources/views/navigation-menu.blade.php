7b2bcd863d5d5056b4e88bf079e2e927e7eaf3e8
gantulgarus <gantulgarus@gmail.com>
2024-03-26 08:59:06 +0800

    update profile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -60,8 +60,7 @@
                                         <img class="h-10 w-10 rounded-full object-cover"
                                             src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
-                                    
-                                @else
+                                @elseif(Auth::user()->danImage != null)
                                 <span class="inline-flex rounded-md">
                                     <button type="button"
                                         class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
@@ -70,6 +69,13 @@
                                             src="data:image/png;base64,{{ Auth::user()->danImage }}" alt="profile">
                                     </button>
                                 </span>
+                                @else
+                                    <button
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        <span class="mr-3">{{ Auth::user()->name }}</span>
+                                        <img class="h-10 w-10 rounded-full object-cover"
+                                            src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
                                 @endif
                             </x-slot>
 
