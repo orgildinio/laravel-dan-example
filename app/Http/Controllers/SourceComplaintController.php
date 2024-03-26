bcc99e849f99b07cf8f2cc28afe0d98afc1a63d2
gantulgarus <gantulgarus@gmail.com>
2024-03-26 13:45:29 +0800

    test 1111 connection

--- app/Http/Controllers/SourceComplaintController.php
+++ app/Http/Controllers/SourceComplaintController.php
@@ -20,34 +20,32 @@ class SourceComplaintController extends Controller
         // $response = Http::get('https://www.11-11.mn/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
         // $response = Http::get('http://103.87.69.87/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
         // dd($response);
-        // $results = $response->json();
-        // Get the response body as a string
         $responseData = $response->getBody()->getContents();
 
         // Convert the JSON response to an array
         $dataArray = json_decode($responseData, true);
-        dd($dataArray);
+        // dd($dataArray);
 
         // // Convert JSON string to PHP array
         // $data = json_decode($results, true);
 
-        // // Initialize an empty array for the converted data
-        // $convertedData = [];
+        // Initialize an empty array for the converted data
+        $convertedData = [];
 
-        // // Iterate through the 'smart' array to create the desired format
-        // foreach ($data['smart']['created_at'] as $key => $value) {
-        //     $convertedData[] = [
-        //         "created_at" => $data['smart']['created_at'][$key],
-        //         "source" => $data['smart']['source'][$key],
-        //         "quarter" => $data['smart']['quarter'][$key],
-        //         // Add other fields here
-        //     ];
-        // }
+        // Iterate through the 'smart' array to create the desired format
+        foreach ($dataArray['smart']['created_at'] as $key => $value) {
+            $convertedData[] = [
+                "created_at" => $dataArray['smart']['created_at'][$key],
+                "source" => $dataArray['smart']['source'][$key],
+                "quarter" => $dataArray['smart']['quarter'][$key],
+                // Add other fields here
+            ];
+        }
 
-        // // Encode the converted data back to JSON format if needed
-        // $convertedJson = json_encode($convertedData, JSON_PRETTY_PRINT);
+        // Encode the converted data back to JSON format if needed
+        $convertedJson = json_encode($convertedData, JSON_PRETTY_PRINT);
 
-        // // Print or return the converted JSON data
-        // return $convertedJson;
+        // Print or return the converted JSON data
+        return $convertedJson;
     }
 }
