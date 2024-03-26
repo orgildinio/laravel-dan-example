76924c8dabe256d93efa6a4a6a1119fbc297dba8
gantulgarus <gantulgarus@gmail.com>
2024-03-26 09:34:40 +0800

    test 1111 connection

--- app/Http/Controllers/SourceComplaintController.php
+++ app/Http/Controllers/SourceComplaintController.php
@@ -9,7 +9,7 @@ class SourceComplaintController extends Controller
 {
     public function sourceComplaints()
     {
-        $response = Http::get('http://103.87.69.87/GS/ServiceE?action=get-tickets&count=100&u=smart_39&p=SmartUser2023*&api_key=0');
+        $response = Http::get('http://103.87.69.87:80/GS/ServiceE?action=get-tickets&count=100&u=smart_39&p=SmartUser2023*&api_key=0');
         dd($response->json());
     }
 }
