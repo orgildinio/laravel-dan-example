fe9aba6b8ed3b490821a02e796c1e11c0664b723
gantulgarus <gantulgarus@gmail.com>
2024-03-26 13:34:58 +0800

    test 1111 connection

--- app/Http/Controllers/SourceComplaintController.php
+++ app/Http/Controllers/SourceComplaintController.php
@@ -19,7 +19,7 @@ class SourceComplaintController extends Controller
         $response = Http::get('https://www.11-11.mn/GStest/APIa', $params);
         // $response = Http::get('https://www.11-11.mn/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
         // $response = Http::get('http://103.87.69.87/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
-        dd($response->getBody()->getContents());
+        dd($response->getBody());
         // $results = $response->json();
 
         // // Convert JSON string to PHP array
