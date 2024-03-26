2445ea2dba07d82859aa1f1438d68fd69a8248e6
gantulgarus <gantulgarus@gmail.com>
2024-03-26 12:31:34 +0800

    test 1111 connection

--- app/Http/Controllers/SourceComplaintController.php
+++ app/Http/Controllers/SourceComplaintController.php
@@ -16,9 +16,9 @@ class SourceComplaintController extends Controller
             'p' => 'OYGNvAnwZ',
             'api_key' => 0
         ];
-        // $response = Http::get('https://www.11-11.mn/GStest/APIa', $params);
+        $response = Http::get('https://www.11-11.mn/GStest/APIa', $params);
         // $response = Http::get('https://www.11-11.mn/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
-        $response = Http::get('http://103.87.69.87/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
+        // $response = Http::get('http://103.87.69.87/GStest/APIa?action=get-tickets&count=30&u=smart_42&p=OYGNvAnwZ&api_key=0');
         dd($response->json());
     }
 }
