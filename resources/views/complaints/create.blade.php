97e7e9e5b564cbe7ca3a3052d7794a12c9f66089
gantulgarus <gantulgarus@gmail.com>
2024-03-27 15:37:52 +0800

    update source complaints

--- resources/views/complaints/create.blade.php
+++ resources/views/complaints/create.blade.php
@@ -113,7 +113,7 @@
                             <div class="md:w-2/3">
                                 <input id="firstname"
                                     class="bg-gray-200 appearance-none rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight @if($errors->has('firstname')) border border-red-500 @else border-1 border-gray-200 @endif"
-                                    type="text" name="firstname" value="">
+                                    type="text" name="firstname" value="{{ request('fullname') }}">
                                 @error('firstname')
                                 <div class="text-red-500 text-sm mt-1 mb-1">{{ $message }}</div>
                                 @enderror
@@ -180,7 +180,7 @@
                         <div class="md:w-2/3">
                             <input id="mail"
                                 class="bg-gray-200 appearance-none rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight @if($errors->has('email')) border border-red-500 @else border-1 border-gray-200 @endif"
-                                type="email" name="email" value="{{old('email')}}">
+                                type="email" name="email" value="{{request('email')}}">
                             @error('email')
                             <div class="text-red-500 text-sm mt-1 mb-1">{{ $message }}</div>
                             @enderror
--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -3,7 +3,7 @@
         @if (count($complaints) > 0)
             @foreach ($complaints as $complaint)
                 <div class="mx-auto border border-gray-200 rounded-lg text-gray-700 mb-0.5 h-30 clickable-row cursor-pointer hover:bg-gray-100"
-                data-url="{{ route('complaint.create', ['fullname' => $complaint['fullname']]) }}">
+                data-url="{{ route('complaint.create') }}" data-fullname="{{ $complaint->fullname }}" data-email="{{ $complaint->email }}">
                     <div class="flex p-3 border-l-4 border-red-500 rounded-lg">
                         <div class="space-y-1 border-r-2 pr-3">
                             <div class="text-xs leading-5 font-semibold"><span
@@ -50,19 +50,28 @@
 @push('scripts')
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        // Add click event listener to all table rows with class 'clickable-row'
-        document.querySelectorAll('.clickable-row').forEach(function (row) {
-            row.addEventListener('click', function () {
-                // Get the data-url attribute value from the clicked row
-                var url = this.getAttribute('data-url');
-                // Redirect to the create page
-                window.location.href = url;
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     // Add click event listener to all table rows with class 'clickable-row'
+    //     document.querySelectorAll('.clickable-row').forEach(function (row) {
+    //         row.addEventListener('click', function () {
+    //             // Get the data-url attribute value from the clicked row
+    //             var url = this.getAttribute('data-url');
+    //             // Redirect to the create page
+    //             window.location.href = url + '?name=' + name
 
-                // Prefill form inputs with parameters
-                var fullname = this.getAttribute('fullname');
-                document.getElementById('fullname').value = fullname;
-            });
+    //             // Prefill form inputs with parameters
+    //             var name = $(this).data('name');
+    //         });
+    //     });
+    // });
+    $(document).ready(function () {
+        $('.clickable-row').click(function () {
+            var url = $(this).data('url');
+            var fullname = $(this).data('fullname');
+            var email = $(this).data('email');
+            
+            // Redirect to create page with data
+            window.location.href = url + '?fullname=' + fullname + '&email=' + email;
         });
     });
 </script>
