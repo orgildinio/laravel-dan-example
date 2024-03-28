bffab2996db34bf31f08fc035e7a58639d48a4d9
gantulgarus <gantulgarus@gmail.com>
2024-03-28 09:28:13 +0800

    create Authentication logs

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $user = $event->user;
+        // Log::info('User logged in: ' . $user->email);
+        $ipAddress = request()->ip(); // Get the user's IP address
+        Log::channel('auth_log')->info('User logged in successfully: ' . $user->email . ' from IP: ' . $ipAddress);
+    }
+}
