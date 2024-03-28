8bcebe005c876247e18b7623ec949859ed5c778c
gantulgarus <gantulgarus@gmail.com>
2024-03-28 15:56:29 +0800

    update source complaint

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -2,8 +2,13 @@
     <div class="bg-white shadow rounded-lg p-4 2xl:col-span-1">
         @if (count($complaints) > 0)
             @foreach ($complaints as $complaint)
-                <div class="mx-auto border border-gray-200 rounded-lg text-gray-700 mb-0.5 h-30 clickable-row cursor-pointer hover:bg-gray-100"
-                data-url="{{ route('complaint.create') }}" data-created="{{ $complaint->created_date }}" data-fullname="{{ $complaint->fullname }}" data-phone="{{ $complaint->phone }}" data-email="{{ $complaint->email }}" data-city="{{ $complaint->city }}" data-district="{{ $complaint->district }}" data-address="{{ $complaint->address }}" data-content="{{ $complaint->content }}" data-number="{{ $complaint->number }}" data-quarter="{{ $complaint->quarter }}">
+                <div class="mx-auto border border-gray-200 rounded-lg text-gray-700 mb-0.5 h-30 @if (isset($complaint['complaint_id'])) 'clickable-row' @else 'disabled' @endif cursor-pointer hover:bg-gray-100"
+                    data-url="{{ route('complaint.create') }}" data-created="{{ $complaint->created_date }}"
+                    data-fullname="{{ $complaint->fullname }}" data-phone="{{ $complaint->phone }}"
+                    data-email="{{ $complaint->email }}" data-city="{{ $complaint->city }}"
+                    data-district="{{ $complaint->district }}" data-address="{{ $complaint->address }}"
+                    data-content="{{ $complaint->content }}" data-number="{{ $complaint->number }}"
+                    data-quarter="{{ $complaint->quarter }}">
                     <div class="flex p-3 border-l-4 border-red-500 rounded-lg">
                         <div class="space-y-1 border-r-2 pr-3">
                             <div class="text-xs leading-5 font-semibold"><span
@@ -18,8 +23,7 @@
                             <div class="ml-3 space-y-1 border-r-2 pr-3">
                                 <div class="text-sm leading-4 font-semibold">
                                     {{ $complaint['fullname'] }}
-                                    <span
-                                        class="text-xs leading-4 font-normal text-gray-500">Иргэн</span>
+                                    <span class="text-xs leading-4 font-normal text-gray-500">Иргэн</span>
                                 </div>
                                 <div class="text-sm leading-4 font-normal">
                                     {{-- {{ Str::limit($complaint['content'], 200) }} --}}
@@ -30,7 +34,7 @@
                         <div>
                             <div class="ml-3 my-5 bg-red-500 p-1 w-20">
                                 <div class="uppercase text-xs leading-4 font-semibold text-center text-white">
-                                    {{ $complaint['type']}}</div>
+                                    {{ $complaint['type'] }}</div>
                             </div>
                             <div class="ml-3 p-1 bg-gray-100 rounded text-xs">
                                 {{ isset($complaint['complaint_id']) ? 'Бүртгэгдсэн' : '' }}
@@ -52,27 +56,30 @@
 
 @push('scripts')
 
-<script>
-    $(document).ready(function () {
-        $('.clickable-row').click(function () {
-            const url = $(this).data('url');
-            const created = $(this).data('created');
-            const fullname = $(this).data('fullname');
-            const phone = $(this).data('phone');
-            const email = $(this).data('email');
-            const city = $(this).data('city');
-            const district = $(this).data('district');
-            const address = $(this).data('address');
-            const content = $(this).data('content');
-            const number = $(this).data('number');
-            const quarter = $(this).data('quarter');
+    <script>
+        $(document).ready(function() {
+            $('.clickable-row').click(function() {
+                const url = $(this).data('url');
+                const created = $(this).data('created');
+                const fullname = $(this).data('fullname');
+                const phone = $(this).data('phone');
+                const email = $(this).data('email');
+                const city = $(this).data('city');
+                const district = $(this).data('district');
+                const address = $(this).data('address');
+                const content = $(this).data('content');
+                const number = $(this).data('number');
+                const quarter = $(this).data('quarter');
 
-            const nameParts = fullname.split(' ');
-            const lastname = nameParts[0];
-            const firstname = nameParts.slice(1).join(' ');
-            
-            // Redirect to create page with data
-            window.location.href = url + '?firstname=' + firstname + '&lastname=' + lastname + '&email=' + email + '&created=' + created + '&phone=' + phone + '&city=' + city + '&district=' + district + '&address=' + address + '&content=' + content + '&number=' + number + '&quarter=' + quarter;
+                const nameParts = fullname.split(' ');
+                const lastname = nameParts[0];
+                const firstname = nameParts.slice(1).join(' ');
+
+                // Redirect to create page with data
+                window.location.href = url + '?firstname=' + firstname + '&lastname=' + lastname +
+                    '&email=' + email + '&created=' + created + '&phone=' + phone + '&city=' + city +
+                    '&district=' + district + '&address=' + address + '&content=' + content + '&number=' +
+                    number + '&quarter=' + quarter;
+            });
         });
-    });
-</script>
+    </script>
