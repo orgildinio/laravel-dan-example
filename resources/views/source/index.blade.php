d5bdd18571e144ee38161239821867f304b297b5
gantulgarus <gantulgarus@gmail.com>
2024-03-28 14:40:33 +0800

    update source complaint

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -32,6 +32,9 @@
                                 <div class="uppercase text-xs leading-4 font-semibold text-center text-white">
                                     {{ $complaint['type']}}</div>
                             </div>
+                            <div class="p-2 bg-gray-100">
+                                {{ $complaint['complaint_id'] }}
+                            </div>
                         </div>
                     </div>
                 </div>
