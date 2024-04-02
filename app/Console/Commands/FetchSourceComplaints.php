450c3aa1955716247b8b0a8cec9e5f7b1886e909
gantulgarus <gantulgarus@gmail.com>
2024-04-02 10:25:35 +0800

    update source complaint create

--- app/Console/Commands/FetchSourceComplaints.php
+++ app/Console/Commands/FetchSourceComplaints.php
@@ -33,7 +33,7 @@ class FetchSourceComplaints extends Command
     {
         $params = [
             'action' => 'get-tickets',
-            'count' => 30,
+            'count' => 50,
             'u' => 'smart_42',
             'p' => 'OYGNvAnwZ',
             'api_key' => 0
