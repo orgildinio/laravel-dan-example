0780ceccbb854ec5af77708e2a87a7940a30e6d1
gantulgarus <gantulgarus@gmail.com>
2024-04-02 23:34:20 +0800

    some update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:fetch-source-complaint')->everyMinute();
+        $schedule->command('command:fetch-source-complaint')->everyTenMinutes();
     }
 
     /**
