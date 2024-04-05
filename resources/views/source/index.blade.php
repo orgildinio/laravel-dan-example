fcbf6faf265994b7b86d83d246b08256d0852d28
gantulgarus <gantulgarus@gmail.com>
2024-04-05 09:55:38 +0800

    update source complaint

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -131,7 +131,7 @@
 
         @if (count($complaints) > 0)
             @foreach ($complaints as $complaint)
-                <div class="mx-auto border border-gray-400 rounded-lg text-gray-700 mb-0.5 h-30 clickable-row cursor-pointer hover:bg-gray-100"
+                <div class="mx-auto border border-gray-300 rounded-lg text-gray-700 mb-0.5 h-30 clickable-row cursor-pointer hover:bg-gray-100"
                     data-url="{{ route('complaint.create') }}" data-created="{{ $complaint->created_date }}"
                     data-fullname="{{ $complaint->fullname }}" data-phone="{{ $complaint->phone }}"
                     data-email="{{ $complaint->email }}" data-city="{{ $complaint->city }}"
