707051359247080572a88d5002f483ec2e03efa6
gantulgarus <gantulgarus@gmail.com>
2024-04-08 15:19:36 +0800

    add cdr_id column to complaints

--- resources/views/complaints/create.blade.php
+++ resources/views/complaints/create.blade.php
@@ -293,7 +293,7 @@
                             </label>
                         </div>
                         <div class="md:w-2/3">
-                            <select name="audio_call"
+                            <select name="cdr_id"
                                 class="bg-gray-200 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500">
                                 @foreach ($audio_calls as $audio_call)
                                 <option value="{{ $audio_call->id }}">{{ $audio_call->calldate . ' - ' . $audio_call->dst . ' - ' . $audio_call->billsec . 'sec' }}</option>
