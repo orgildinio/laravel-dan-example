dcd263261e004011be5b5015a72feba7962730d2
gantulgarus <gantulgarus@gmail.com>
2024-04-09 13:52:53 +0800

    some update

--- resources/views/complaints/edit.blade.php
+++ resources/views/complaints/edit.blade.php
@@ -265,6 +265,23 @@
                         </div>
                     </div>
 
+                    <div class="md:flex md:items-center mb-2" id="audio_call">
+                        <div class="md:w-1/3">
+                            <label class="block text-gray-500 text-sm font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                for="inline-full-name">
+                                Яриа бичлэг сонгох
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <select name="cdr_id"
+                                class="bg-gray-200 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500">
+                                @foreach ($audio_calls as $audio_call)
+                                <option value="{{ $audio_call->id }}" @selected($audio_call->id==$complaint->cdr_id)>{{ $audio_call->calldate . ' - ' . $audio_call->src . ' - ' . $audio_call->billsec . 'sec' }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
                     <div class="md:flex md:items-center mb-2">
                         <div class="md:w-1/3">
                         </div>
