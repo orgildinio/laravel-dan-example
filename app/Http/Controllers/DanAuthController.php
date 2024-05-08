a81c5c77b428147fbea7ba6c8490805cf3d649a2
gantulgarus <gantulgarus@gmail.com>
2024-05-08 11:10:17 +0800

    dan org test

--- app/Http/Controllers/DanAuthController.php
+++ app/Http/Controllers/DanAuthController.php
@@ -14,15 +14,18 @@ class DanAuthController extends Controller
     {
 
         if ($type == 'citizen') {
-            $json = '[{"services": ["WS100101_getCitizenIDCardInfo1"], "wsdl": "https://xyp.gov.mn/citizen-1.3.0/ws?WSDL"}]';
+            $json = '[{"services": ["WS100101_getCitizenIDCardInfo"], "wsdl": "https://xyp.gov.mn/citizen-1.3.0/ws?WSDL"}]';
 
             $scope = base64_encode($json);
-        } else {
+        } elseif ($type == 'org') {
             // $json = '[{"services": ["WS100301_getLegalEntityInfo"], "wsdl": "https://xyp.gov.mn/legal-entity-1.3.0/ws?WSDL"}]';
             $json = '[{"services": "dddd", "wsdl": "dddd"}]';
 
             $scope = base64_encode($json);
+        } else {
+            $scope = null;
         }
+        dd($scope);
 
 
         return Socialite::driver('dan')->scopes($scope)->redirect();
