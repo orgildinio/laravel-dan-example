6fa17f59e1e535785651071a1bc8a07431280fd3
gantulgarus <gantulgarus@gmail.com>
2024-05-08 10:55:42 +0800

    dan org test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                                 </span>
                             </a>
 
-                            <a href="{{ route('danlogin', ['provider' => 'org']) }}"
+                            <a href="{{ route('danlogin', ['type' => 'org']) }}"
                                 class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-gray-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
                                 <div class="bg-white p-1 rounded-full">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
