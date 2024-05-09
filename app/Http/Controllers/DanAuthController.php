2c0e96e67fece2af508c1254d3a57c538ce45382
gantulgarus <gantulgarus@gmail.com>
2024-05-09 10:35:18 +0800

    dan test

--- app/Http/Controllers/DanAuthController.php
+++ app/Http/Controllers/DanAuthController.php
@@ -68,7 +68,7 @@ class DanAuthController extends Controller
 
             if (!$user) {
                 // If the user doesn't exist, create a new user in your database
-                session(['first_dan_login' => true]);
+                // session(['first_dan_login' => true]);
                 $user = User::create([
                     'name' => $danUser->companyName,
                     'companyName' => $danUser->companyName . " " . $danUser->description,
