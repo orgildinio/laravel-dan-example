2ed8443c825f243c759fca0cf400b4e4037e830f
gantulgarus <gantulgarus@gmail.com>
2024-07-04 14:11:24 +0800

    update guide

--- resources/views/user-guide.blade.php
+++ resources/views/user-guide.blade.php
@@ -11,7 +11,7 @@
             <div class="bg-white mb-10">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="container">
-                        <iframe src="{{ asset('image/USERguide20240703.pdf') }}" class="pdf-container"></iframe>
+                        <iframe src="{{ asset('image/USERguide20240704.pdf') }}" class="pdf-container"></iframe>
                     </div>
                 </div>
             </div>
