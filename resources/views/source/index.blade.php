e7d697e2203a5540ab4bf53cafbdbc9d9c49a851
gantulgarus <gantulgarus@gmail.com>
2024-09-24 10:31:03 +0800

    fix error

--- resources/views/source/index.blade.php
+++ resources/views/source/index.blade.php
@@ -142,7 +142,7 @@
 
         @if (count($complaints) > 0)
             @foreach ($complaints as $complaint)
-                <div class="mx-auto border border-gray-300 rounded-lg text-gray-700 mb-0.5 h-30 clickable-row cursor-pointer hover:bg-gray-100"
+                <div class="mx-auto border border-gray-300 rounded-lg text-gray-700 mb-0.5 h-30"
                     data-url="{{ route('complaint.create') }}" data-created="{{ $complaint->created_date }}"
                     data-fullname="{{ $complaint->fullname }}" data-phone="{{ $complaint->phone }}"
                     data-email="{{ $complaint->email }}" data-city="{{ $complaint->city }}"
@@ -150,36 +150,40 @@
                     data-content="{{ $complaint->content }}" data-number="{{ $complaint->number }}"
                     data-quarter="{{ $complaint->quarter }}" data-type="{{ $complaint->type }}">
                     <div class="flex p-3 border-l-4 {{ getBorderColor($complaint->type) }} rounded-lg">
-                        <div class="space-y-1 border-r-2 pr-3">
-                            <div class="text-xs leading-5 font-semibold"><span
-                                    class="text-xs leading-4 font-normal text-gray-500"> №</span>
-                                {{ $complaint->number }}</div>
-                            <div class="text-xs leading-5"><span
-                                    class="text-xs leading-4 font-normal text-gray-500 pr">
-                                    Төрөл: </span> {{ $complaint->type }}</div>
-                            <div class="text-xs leading-5"><span class="text-xs leading-4 font-normal text-gray-500">
-                                    Шинээр ирсэн: </span>{{ $complaint->created_date }}</div>
-                        </div>
-                        <div class="flex-1">
-                            <div class="ml-3 space-y-1 border-r-2 pr-3">
-                                <div class="text-sm leading-4 font-semibold">
-                                    {{ $complaint->fullname }}
-                                    <span class="text-xs leading-4 font-normal text-gray-500">Иргэн</span>
-                                </div>
-                                <div class="text-sm leading-4 font-normal">
-                                    {{-- {{ Str::limit($complaint['content'], 200) }} --}}
-                                    {{-- {{ html_entity_decode($complaint['content'], ENT_QUOTES, 'UTF-8') }} --}}
-                                    {{ $complaint->content }}
+                        <div class="clickable-row cursor-pointer hover:bg-gray-100">
+                            <div class="space-y-1 border-r-2 pr-3">
+                                <div class="text-xs leading-5 font-semibold"><span
+                                        class="text-xs leading-4 font-normal text-gray-500"> №</span>
+                                    {{ $complaint->number }}</div>
+                                <div class="text-xs leading-5"><span
+                                        class="text-xs leading-4 font-normal text-gray-500 pr">
+                                        Төрөл: </span> {{ $complaint->type }}</div>
+                                <div class="text-xs leading-5"><span class="text-xs leading-4 font-normal text-gray-500">
+                                        Шинээр ирсэн: </span>{{ $complaint->created_date }}</div>
+                            </div>
+                            <div class="flex-1">
+                                <div class="ml-3 space-y-1 border-r-2 pr-3">
+                                    <div class="text-sm leading-4 font-semibold">
+                                        {{ $complaint->fullname }}
+                                        <span class="text-xs leading-4 font-normal text-gray-500">Иргэн</span>
+                                    </div>
+                                    <div class="text-sm leading-4 font-normal">
+                                        {{-- {{ Str::limit($complaint['content'], 200) }} --}}
+                                        {{-- {{ html_entity_decode($complaint['content'], ENT_QUOTES, 'UTF-8') }} --}}
+                                        {{ $complaint->content }}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <div class="ml-3 my-5 {{ getBgColor($complaint->type) }} p-1 w-20">
-                                <div class="uppercase text-xs leading-4 font-semibold text-center text-white">
-                                    {{ $complaint->type }}</div>
-                            </div>
-                            <div class="ml-3 p-1 bg-gray-100 rounded text-xs">
-                                <a class="" onclick="return confirm('1111-рүү Өргөдөл, гомдол буцаах уу?')" href="{{route('unreceipt', $complaint->id)}}">Буцаах</a>
+                            <div>
+                                <div class="ml-3 my-5 {{ getBgColor($complaint->type) }} p-1 w-20">
+                                    <div class="uppercase text-xs leading-4 font-semibold text-center text-white">
+                                        {{ $complaint->type }}</div>
+                                </div>
+                                <div class="ml-3 p-1 bg-gray-100 rounded text-xs">
+                                    <a class="" onclick="return confirm('1111-рүү Өргөдөл, гомдол буцаах уу?')" href="{{route('unreceipt', $complaint->id)}}">Буцаах</a>
+                                </div>
                             </div>
                         </div>
                     </div>
