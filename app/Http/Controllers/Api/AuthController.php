056dab53a948daf9e0abae6459be8d0fcad613af
gantulgarus <gantulgarus@gmail.com>
2024-10-20 01:15:02 +0800

    update api for docs

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\LoginApiRequest;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -154,6 +155,63 @@ class AuthController extends Controller
         return response()->json([
             'token' => $token,
             'user' => $user,
+        ]);
+    }
+
+    public function loginTze(Request $request)
+    {
+        // Validate request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        // Validate request
+        // $request->validate([
+        //     'email' => 'required|email',
+        //     'password' => 'required',
+        // ]);
+
+        // Check if the email and password match any user
+        if (!Auth::attempt($credentials)) {
+            // Check if email exists
+            $user = User::where('email', $request->email)->first();
+            if ($user && !Hash::check($request->password, $user->password)) {
+                return response()->json(['error' => 'Нууц үг буруу'], 401);
+            }
+
+            return response()->json(['error' => 'Мэйл хаяг эсвэл нууц үг буруу байна.'], 401);
+        }
+
+        // Generate token and return response
+        $user = Auth::user();
+
+        $token = $user->createToken('Personal Access Token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Амжилттай нэвтэрлээ!',
+            'token' => $token,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'division' => $user->division,
+                'phone' => $user->phone,
+                'organization' => $user->org?->name,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+            ]
         ]);
     }
 }
--- app/Http/Controllers/Api/ComplaintController.php
+++ app/Http/Controllers/Api/ComplaintController.php
@@ -10,6 +10,7 @@ use App\Models\Complaint;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ComplaintResource;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ComplaintController extends Controller
@@ -89,14 +90,14 @@ class ComplaintController extends Controller
             // Return the newly created complaint using a resource
             return response()->json([
                 'status' => 'success',
-                'message' => 'Complaint created successfully',
+                'message' => 'Өргөдөл, гомдол амжилттай бүртгэлээ',
                 'data' => new ComplaintResource($complaint)
             ], 201);
         } catch (Exception $e) {
             // Handle any exceptions that occur during the complaint creation
             return response()->json([
                 'status' => 'failed',
-                'message' => 'An error occurred while creating the complaint',
+                'message' => 'Өргөдөл, гомдол бүртгэхэд алдаа гарлаа',
                 'error' => $e->getMessage()
             ], 500);
         }
@@ -152,5 +153,77 @@ class ComplaintController extends Controller
         $complaint->delete();
 
         return response(null, 204);
+    }
+
+    public function storeTze(Request $request)
+    {
+        try {
+
+            // Custom validation rules
+            $request->validate([
+                'lastname' => 'required|string|min:2|max:50',
+                'firstname' => 'required|string|min:2|max:50',
+                // 'registerNumber' => 'required|string|min:8|max:12|regex:/^[0-9]+$/',
+                'country' => 'required|string|max:100',
+                'district' => 'required|string|max:100',
+                'khoroo' => 'required|string|max:100',
+                // 'addressDetail' => 'required|string|max:255',
+                'phone' => [
+                    'required',
+                    'regex:/^(\+976|976)?([89]{1}[0-9]{7})$/'
+                ],
+                // 'email' => 'required|email|max:255',
+                'complaint' => 'required',
+            ]);
+
+            // If validation passes, proceed
+            $user = Auth::user();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Нэвтрээгүй хэрэглэгч байна. Токеныг шалгана уу.'
+                ], 401);
+            }
+
+            // Continue with the rest of the logic
+            $input = $request->all();
+            $input['complaint_date'] = Carbon::now();
+            $input['expire_date'] = Carbon::now()->addHours(48);
+            $input['created_user_id'] = $user->id;
+            $input['organization_id'] = $user->org->id;
+            $input['energy_tyoe_id'] = $user->org->plant_id;
+            $input['status_id'] = 0; // Шинээр ирсэн
+            $input['channel_id'] = 1; // Веб
+            $input['complaint_maker_type_id'] = 0; // Иргэн
+
+            $complaint = Complaint::create($input);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Өргөдөл, гомдол амжилттай бүртгэлээ',
+                // 'data' => new ComplaintResource($complaint)
+                'complaint' => [
+                    'id' => $complaint->id,
+                    'serial_number' => $complaint->serial_number,
+                    'created_at' => $user->created_at,
+                    'updated_at' => $user->updated_at,
+                ]
+            ], 201);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Return the validation errors in JSON format
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Validation errors occurred',
+                'errors' => $e->errors()
+            ], 422);
+        } catch (Exception $e) {
+            // Handle other exceptions
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Өргөдөл, гомдол бүртгэхэд алдаа гарлаа',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
