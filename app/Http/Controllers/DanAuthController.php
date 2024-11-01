d4b65fc50703c6d16723d2706d6d062c253828e2
gantulgarus <gantulgarus@gmail.com>
2024-11-01 16:14:17 +0800

    update dan auth login

--- app/Http/Controllers/DanAuthController.php
+++ app/Http/Controllers/DanAuthController.php
@@ -33,7 +33,7 @@ class DanAuthController extends Controller
 
     public function handleDanCallback()
     {
-        dd(Socialite::driver('dan')->user());
+        dd(Socialite::driver('dan'));
 
         $danUser = Socialite::driver('dan')->user();
 
