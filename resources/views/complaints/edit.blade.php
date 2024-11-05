ce079afcc7a9bcaa5f4af30ef545591face35e7c
gantulgarus <gantulgarus@gmail.com>
2024-11-05 15:27:58 +0800

    some update

--- resources/views/complaints/edit.blade.php
+++ resources/views/complaints/edit.blade.php
@@ -258,7 +258,7 @@
                             </label>
                         </div>
                         <div class="md:w-3/4">
-                            <select name="channel_id"
+                            <select name="channel_id" id="channel_id"
                                 class="bg-gray-100 appearance-none border-1 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-indigo-500">
                                 @foreach ($channels as $channel)
                                 <option value="{{ $channel->id }}" @selected($channel->id==$complaint->channel_id)>{{
