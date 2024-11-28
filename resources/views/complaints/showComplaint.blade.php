1a753757c7c884e23b9a40c4823f17730c0e355e
gantulgarus <gantulgarus@gmail.com>
2024-11-28 11:52:53 +0800

    test

--- resources/views/complaints/showComplaint.blade.php
+++ resources/views/complaints/showComplaint.blade.php
@@ -51,20 +51,6 @@
                                         @if ($complaint->file_id != null)
                                         <x-file-list-component :$fileName :$fileExt :$fileUrl :$fileSizeInKilobytes />
                                         @endif
-                                        <p>file name {{ $fileName }}</p>
-                                        <p>file ext {{ $fileExt }}</p>
-                                        <p>file fileUrl {{ $fileUrl }}</p>
-                                        <p>file fileSizeInKilobytes {{ $fileSizeInKilobytes }}</p>
-                                        @if ($complaint->files->isNotEmpty())
-                                            <div class="flex flex-row flex-wrap">
-                                                @foreach ($complaint->files as $file)
-                                                <div class="m-2">
-                                                        <x-file-list-component :fileName="$file->filename" :fileExt="pathinfo($file->filename, PATHINFO_EXTENSION)" :fileUrl="url('files/' . $file->filename)"
-                                                            :fileSizeInKilobytes="10" />
-                                                        </div>
-                                                    @endforeach
-                                            </div>
-                                        @endif
 
                                         @if ($complaint->audio_file_id != null)
                                             <div class="text-sm px-2">
