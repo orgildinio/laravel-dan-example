d69e8c5edd5c0954c04fba2511a5fff3d8283549
gantulgarus <gantulgarus@gmail.com>
2025-01-24 15:53:50 +0800

    update report by channel

--- resources/views/reports/report1.blade.php
+++ resources/views/reports/report1.blade.php
@@ -1,84 +1,3 @@
-<style>
-    html {
-        font-family: sans-serif;
-    }
-
-    table {
-        border-collapse: collapse;
-        width: 100%;
-        border: 2px solid rgb(200, 200, 200);
-        letter-spacing: 1px;
-        font-size: 0.8rem;
-    }
-
-    th, td {
-        border: 1px solid rgb(190, 190, 190);
-        padding: 10px;
-        text-align: center;
-    }
-
-    th {
-        background-color: rgb(235, 235, 235);
-    }
-
-    tr:nth-child(even) {
-        background-color: rgb(245, 245, 245);
-    }
-
-    tr:nth-child(odd) {
-        background-color: rgb(250, 250, 250);
-    }
-
-    tr:hover {
-        background-color: #ddd;
-    }
-
-    caption {
-        padding: 10px;
-        font-size: 1.1rem;
-        font-weight: bold;
-    }
-
-    .vertical-text {
-        transform: rotate(-90deg);
-        width: 30px;
-        white-space: nowrap;
-    }
-
-    .v-header {
-        height: 120px;
-        vertical-align: middle;
-    }
-
-    .tulbur {
-        background-color: lightgreen;
-    }
-
-    .chanar {
-        background-color: lightpink;
-    }
-
-    .hemjih {
-        background-color: lightyellow;
-    }
-
-    .hariltsaa {
-        background-color: lightblue;
-    }
-
-    .busad {
-        background-color: lightsteelblue;
-    }
-
-    .halaalt {
-        background-color: yellow;
-    }
-
-    .haluunus {
-        background-color: orange;
-    }
-
-</style>
 <x-admin-layout>
     <h3 class="text-xl font-bold">Тайлан гомдлын төрөл болон хүлээн авсан суваг</h3>
     <div class="w-full">
@@ -105,6 +24,16 @@
                         @endforeach
                     </select>
                 </div>
+                <div class="mr-1">
+                    <select name="complaint_type_id" id="complaint_type_id"
+                        class="w-32 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2">
+                        @foreach ($complaint_types as $type)
+                            <option value="{{ $type->id }}"
+                                {{ old('complaint_type_id', $complaint_type_id) == $type->id ? 'selected' : '' }}>
+                                {{ $type->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
                 <div class="flex flex-row items-center space-x-2">
                     <button type="submit"
                         class="flex items-center justify-center text-white bg-primary hover:bg-primaryHover focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2">
@@ -115,305 +44,34 @@
             </div>
         </form>
     </div>
-    <table id="report1">
-        <thead>
+    <table id="report1" class="table-auto w-full bg-white border border-gray-300">
+        <thead class="bg-primary text-white">
             <tr>
-                <th rowspan="3">№</th>
-                <th rowspan="3" style="white-space: nowrap; text-align: left;">Тусгай зөвшөөрөл эзэмшигч</th>
-                <th colspan="63" style="background-color: gray; color: white;">Өргөдөл, гомдлын төрөл</th>
-            </tr>
-            <tr>
-                <th colspan="8" class="tulbur">Төлбөр тооцоо</th>
-                <th colspan="8" class="chanar">Чанар, хангамж</th>
-                <th colspan="8" class="hemjih">Хэмжих хэрэгсэл</th>
-                <th colspan="8" class="hariltsaa">Харилцаа, ёс зүй</th>
-                <th colspan="8" class="busad">Бусад</th>
-                @if ($energy_type_id == 2)    
-                <th colspan="8" class="halaalt">Халаалт</th>
-                <th colspan="8" class="haluunus">Халуун ус</th>
-                @endif
-                <th colspan="8">Нийт</th>
-            </tr>
-            <tr>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header tulbur">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                <th class="v-header chanar">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header chanar">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header hemjih">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header hariltsaa">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                <th class="v-header busad">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header busad">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                @if ($energy_type_id == 2)   
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header halaalt">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">1111</div>
-                </th>
-                <th class="v-header haluunus">
-                    <div class="vertical-text">Нийт</div>
-                </th>
-                @endif
-
-                <th class="v-header">
-                    <div class="vertical-text">Веб</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">Имэйл</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">Биечлэн</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">Гар утас</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">Бичгээр</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">Утсаар</div>
-                </th>
-                <th class="v-header">
-                    <div class="vertical-text">1111</div>
-                </th>
-
+                <th class="border border-gray-300 px-4 py-2">№</th>
+                <th class="border border-gray-300 px-4 py-2">Тусгай зөвшөөрөл эзэмшигч</th>
+                <th class="border border-gray-300 px-4 py-2">Веб</th>
+                <th class="border border-gray-300 px-4 py-2">Имэйл</th>
+                <th class="border border-gray-300 px-4 py-2">Биечлэн</th>
+                <th class="border border-gray-300 px-4 py-2">Гар утас</th>
+                <th class="border border-gray-300 px-4 py-2">Бичгээр</th>
+                <th class="border border-gray-300 px-4 py-2">Утсаар</th>
+                <th class="border border-gray-300 px-4 py-2">1111</th>
+                <th class="border border-gray-300 px-4 py-2">Нийт</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($reportData as $data)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td style="white-space: nowrap; text-align: left;">{{ $data->name }}</td>
-
-                    <td>{{ $data->type1_channel1_count }}</td>
-                    <td>{{ $data->type1_channel2_count }}</td>
-                    <td>{{ $data->type1_channel3_count }}</td>
-                    <td>{{ $data->type1_channel4_count }}</td>
-                    <td>{{ $data->type1_channel5_count }}</td>
-                    <td>{{ $data->type1_channel6_count }}</td>
-                    <td>{{ $data->type1_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type1_count_all }}</td>
-
-                    <td>{{ $data->type2_channel1_count }}</td>
-                    <td>{{ $data->type2_channel2_count }}</td>
-                    <td>{{ $data->type2_channel3_count }}</td>
-                    <td>{{ $data->type2_channel4_count }}</td>
-                    <td>{{ $data->type2_channel5_count }}</td>
-                    <td>{{ $data->type2_channel6_count }}</td>
-                    <td>{{ $data->type2_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type2_count_all }}</td>
-
-                    <td>{{ $data->type3_channel1_count }}</td>
-                    <td>{{ $data->type3_channel2_count }}</td>
-                    <td>{{ $data->type3_channel3_count }}</td>
-                    <td>{{ $data->type3_channel4_count }}</td>
-                    <td>{{ $data->type3_channel5_count }}</td>
-                    <td>{{ $data->type3_channel6_count }}</td>
-                    <td>{{ $data->type3_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type3_count_all }}</td>
-
-                    <td>{{ $data->type4_channel1_count }}</td>
-                    <td>{{ $data->type4_channel2_count }}</td>
-                    <td>{{ $data->type4_channel3_count }}</td>
-                    <td>{{ $data->type4_channel4_count }}</td>
-                    <td>{{ $data->type4_channel5_count }}</td>
-                    <td>{{ $data->type4_channel6_count }}</td>
-                    <td>{{ $data->type4_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type4_count_all }}</td>
-
-                    <td>{{ $data->type5_channel1_count }}</td>
-                    <td>{{ $data->type5_channel2_count }}</td>
-                    <td>{{ $data->type5_channel3_count }}</td>
-                    <td>{{ $data->type5_channel4_count }}</td>
-                    <td>{{ $data->type5_channel5_count }}</td>
-                    <td>{{ $data->type5_channel6_count }}</td>
-                    <td>{{ $data->type5_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type5_count_all }}</td>
-
-                    @if ($energy_type_id == 2)   
-                    <td>{{ $data->type6_channel1_count }}</td>
-                    <td>{{ $data->type6_channel2_count }}</td>
-                    <td>{{ $data->type6_channel3_count }}</td>
-                    <td>{{ $data->type6_channel4_count }}</td>
-                    <td>{{ $data->type6_channel5_count }}</td>
-                    <td>{{ $data->type6_channel6_count }}</td>
-                    <td>{{ $data->type6_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type6_count_all }}</td>
-
-                    <td>{{ $data->type7_channel1_count }}</td>
-                    <td>{{ $data->type7_channel2_count }}</td>
-                    <td>{{ $data->type7_channel3_count }}</td>
-                    <td>{{ $data->type7_channel4_count }}</td>
-                    <td>{{ $data->type7_channel5_count }}</td>
-                    <td>{{ $data->type7_channel6_count }}</td>
-                    <td>{{ $data->type7_channel7_count }}</td>
-                    <td style="font-weight: bold;">{{ $data->type7_count_all }}</td>
-                    @endif
-
-                    <td style="font-weight: bold;">{{ $data->channel1_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel2_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel3_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel4_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel5_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel6_all }}</td>
-                    <td style="font-weight: bold;">{{ $data->channel7_all }}</td>
+                <tr class="hover:bg-blue-100">
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $loop->iteration }}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{ $data->name }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel1_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel2_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel3_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel4_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel5_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel6_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-center">{{ $data->channel7_all }}</td>
+                    <td class="border border-gray-300 px-4 py-2 font-bold text-center">{{ $data->total_channels }}</td>
                 </tr>
             @endforeach
         </tbody>
